8b86606bd7e0091cbf913db9f27252ba0bf34e9b
JulianDavidBuitrago <jdborozco90@gmail.com>
2020-02-18 21:14:20 -0500

    CAMBIO

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -15,7 +15,9 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("HELLO WORLD");
+        System.out.println("HELLO WORLD!!");
+        System.out.println("GOOD BYE WORLD!!");
+        
     }
     
 }
